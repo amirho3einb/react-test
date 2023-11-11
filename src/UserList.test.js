54d141a72e19a9251d62890416889a1f4f16371b
Amirhossein <amirhossinbalagar@gmail.com>
2023-11-11 11:50:39 +0330

    Another Query Function Fallback #querySelectorAll

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -7,14 +7,14 @@ test("render one row per user", async () => {
     { name: "Sama", email: "sama@gmail.com" },
     { name: "Amirhossein", email: "amirhossin@gmail.com" },
   ];
-  render(<UserList users={users} />);
+  const { container } = render(<UserList users={users} />);
 
   // Find all the rows in the table
-  const rows = within(screen.getByTestId("users")).getAllByRole('row');
+  // const rows = within(screen.getByTestId("users")).getAllByRole("row");
+  const rows = container.querySelectorAll("tbody tr");
 
   // Assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
 });
 
 test("render the email and name of each user", async () => {});
-
